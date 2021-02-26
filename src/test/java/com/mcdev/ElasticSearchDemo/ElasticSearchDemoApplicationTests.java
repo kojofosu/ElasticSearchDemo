90bf8e2c1e2883a927fd0559812ef9676e90aef4
kojofosu <kojofosube@gmail.com>
2021-02-26 13:17:08 +0000

    project initialised

---
+++ src/test/java/com/mcdev/ElasticSearchDemo/ElasticSearchDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcdev.ElasticSearchDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticSearchDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
