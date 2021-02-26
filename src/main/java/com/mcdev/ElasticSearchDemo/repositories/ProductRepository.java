82fd123b118278e9e0071fe4ecebaf2204a30469
kojofosu <kojofosube@gmail.com>
2021-02-26 14:30:30 +0000

    added repository and document

---
+++ src/main/java/com/mcdev/ElasticSearchDemo/repositories/ProductRepository.java
@@ -0,0 +1,16 @@
+package com.mcdev.ElasticSearchDemo.repositories;
+
+import com.mcdev.ElasticSearchDemo.Product;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+import java.util.List;
+
+public interface ProductRepository extends ElasticsearchRepository<Product, String> {
+
+    List<Product> findByName(String name);
+
+    List<Product> findByNameContaining(String name);
+
+    List<Product> findByManufacturerAndCategory(String manufacturer, String category);
+
+}
